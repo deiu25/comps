63ed1ce52ec0fcc1b6714f44def0dcd638070309
andrei badulescu <andrei.badulescu@aplast.eu>
2023-07-11 14:56:09 +0300

    some tailwind

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,30 +1,33 @@
-import { useState } from 'react'
-import { GoPlus, GoDash } from 'react-icons/go'
+import { useState } from "react";
+import { GoPlus, GoDash } from "react-icons/go";
 
 export const Accordion = ({ items }) => {
+  const [expandedIndex, setExpandedIndex] = useState(0);
 
-    const [expandedIndex, setExpandedIndex] = useState(0)
+  const handleClick = (nextIndex) => {
+    setExpandedIndex(nextIndex);
+  };
 
-    const handleClick = (nextIndex) => {
-        setExpandedIndex(nextIndex)
-    }
+  const rederedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
 
-    const rederedItems = items.map((item, index) => {
-        const isExpanded = index === expandedIndex
-        
-        const icon = <span>
-            {isExpanded ? <GoDash /> : <GoPlus /> }
-        </span>
+    const icon = <span className="text-2xl">{isExpanded ? <GoDash /> : <GoPlus />}</span>;
 
-        return (
-            <div key={item.id}>
-            <div onClick={() => handleClick(index)}>{icon}{item.label}</div>
-            <div>{isExpanded && <div>{item.content}</div>}</div>
-            </div>
-        )
-    })
+    return (
+      <div key={item.id}>
+        <div
+          className="flex justify-between p-3 gb-gray-50 border-b items-center cursor-pointer"
+          onClick={() => handleClick(index)}
+        >
+          {item.label}
+          {icon}
+        </div>
+        <div>
+          {isExpanded && <div className="border-b p-5">{item.content}</div>}
+        </div>
+      </div>
+    );
+  });
 
-  return <div>
-        {rederedItems}
-  </div>
-}
+  return <div className="border-x border-t rouded">{rederedItems}</div>;
+};
